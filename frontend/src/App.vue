38d7bd8e68252ff7f81132d4b8fdea55d430081d
Sujeet Kumar Manjhi <sujits32@gmail.com>
2024-06-12 11:54:25 +0530

    add: login component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <n-button>naive-ui</n-button>
   <main>
     <RouterView />
   </main>
 </template>
 
-<script setup>
-import { NButton } from "naive-ui";
-</script>
+<script setup></script>
 
 <style scoped></style>
